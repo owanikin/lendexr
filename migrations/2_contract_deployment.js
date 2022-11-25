93f2c140ea735ad711e7898ebef7a63eb17c5c2e
Owanikin <oderindeife@gmail.com>
2022-11-25 14:19:47 +0100

    migration: deployment

---
+++ migrations/2_contract_deployment.js
@@ -0,0 +1,15 @@
+const Coin = artifacts.require("StableCoinToken")
+const MockOracle = artifacts.require("MockOracle")
+const chainlinkOracle = artifacts.require("PriceConsumerV3");
+const Vault = artifacts.require("Vault")
+
+module.exports = async function (deployer) {
+    await deployer.deploy(Coin, "Lendex Stable", "lxUSD")
+    const coin = await Coin.deployed()
+    await deployer.deploy(MockOracle)
+    const mOracle = await MockOracle.deployed()
+    await deployer.deploy(ChainlinkOracle)
+    const oracle = await ChainlinkOracle.deployed();
+    await deployer.deploy(Vault, coin.address, oracle.address);
+    const vault = await Vault.deployed()
+}
